76239659b9ee4556cfcd815dbb25d6f4081e3bc0
giorgi-BOXING <eka_basharuli@mail.ru>
2025-03-11 15:28:30 +0400

    7th commit

---
+++ day 8/classwork/classwork.py
@@ -0,0 +1,24 @@
+# 1)
+Name = "Nika"
+naMe = "Luka"
+NAME = "Data"
+naME = "Vano"
+name = "Demetre"
+NaMe = "Jan"
+name = "Giorgi"
+
+# რამდენი ცვლადი შეიქმნება ამ კოდის მიხედვით?(აღწერეთ რატომ)
+# აქ არის 5 განსხვავებული ცვლადი რადან პითონი არის case sensitive ენა რაც ნიშნავს იმას რომ დიდ და პატარა ასოებზე არის მგრძობიარე მაგალითად name და NAme არის განსხვავებული ცვლადის სახელები
+
+# 2)
+# შექმენით 5 ცვლადი, snake_case-ის გამოყენებით და დაბეჭდეთ 
+my_name="giorgi"
+my_surname="ugrexelidze"
+my_age=9
+my_favorite_food="ხინკალი"
+my_favorite_teacher="nika keshelava giorgi bibilashvili nika kvaracxelia"
+print(my_name)
+print(my_surname)
+print(my_age)
+print(my_favorite_food)
+print(my_favorite_teacher)
---
+++ day 8/homework/homework 5.py
@@ -0,0 +1,14 @@
+# N1 snace_case არსი ქვედატირიანი სიტყვა აი მაგალითად snace_case აი ეს 
+
+
+# N2 რას გამოიტანს შემდეგი კოდი და რატომ
+
+number1 = 10
+number2 = 20
+sum_of_numbers = number1 + number2
+print(sum_Of_numbers)
+# ეს გამოიატანს 30 იმიტომ რომ მე ვპრინტავ რიცხვების ჯამს                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+
+
+
+
