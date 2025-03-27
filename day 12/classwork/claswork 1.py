70b63c3ae2fe94a2ca34dc3d614a3e3caa23b37e
giorgi-BOXING <eka_basharuli@mail.ru>
2025-03-27 16:46:39 +0400

    commit

---
+++ day 12/classwork/claswork 1.py
@@ -0,0 +1,4 @@
+num1=input("შემოიტანეთ რიცხვი")
+print(type(num1))
+print(int(num1))
+print(float(num1))
---
+++ day 12/homework/homework 2.py
@@ -0,0 +1,6 @@
+num1=20 # ეს არის ინტეჯერი
+num2="30" # ეს არის სტრინგი
+print(num1+int(num2))
+print(num1-int(num2))
+print(num1/int(num2))
+print(num1*int(num2))
